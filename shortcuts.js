a3075e7d5cb979c36b6f251fc7a2119a204c1279
tstordalen <>
2026-01-26 15:19:49 +0100

    Extract inline SVGs to icon files
    - Created question-circle.svg (help button, white for header)
    - Created font-size.svg (editor font slider label)
    - Replaced inline SVGs with <img> references in:
      - index.html: help, font size, play, pause, step, reset buttons
      - shortcuts.js: reset to defaults button
      - main.js: delete level button

--- shortcuts.js
+++ shortcuts.js
@@ -158,10 +158,7 @@ function createShortcuts(storageKey) {
             <span>Enable shortcuts</span>
             <span class="hint">Click to toggle, click key to change</span>
             <button class="shortcuts-reset" title="Reset to defaults">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"/>
-                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"/>
-                </svg>
+                <img src="icons/arrow-counterclockwise.svg" alt="" width="16" height="16">
                 Reset
             </button>
         `;
